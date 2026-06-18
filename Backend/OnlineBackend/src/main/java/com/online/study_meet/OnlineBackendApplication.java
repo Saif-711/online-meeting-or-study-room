0646e7437e4f839e4c745b737de1b75f86f072ae
Saif-711 <saifnairat23@gmail.com>
2026-06-18 13:16:25 +0100

    setup backend

---
+++ Backend/OnlineBackend/src/main/java/com/online/study_meet/OnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.online.study_meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/OnlineBackend/src/test/java/com/online/study_meet/OnlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.study_meet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
